78669e2dc9b46dc640af06f9a86666494075f1eb
syedabdulrehman1 <fast33210@gmail.com>
2024-12-29 23:49:14 +0500

    fixes

--- Frontend/src/Api/axiosInstance.js
+++ Frontend/src/Api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:8000",
+  baseURL: import.meta.env.VITE_BACKEND_URL || "https://fms-f.vercel.app/",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
