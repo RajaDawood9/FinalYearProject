2db073ebdd2c9278e51695393f0b966a2522fb0b
Raja Dawood <143741655+RajaDawood9@users.noreply.github.com>
2025-01-07 22:31:16 +0500

    new

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
     <header className="flex items-center justify-between shadow-xl w-full z-[999] sticky top-0 left-0 right-0 bg-white">
       <div className="rounded-lg">
         <img
-          src= "https://www.hec.gov.pk/english/universities/PublishingImages/NCBAE-Logo.jpg"
+          src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJluxW7GzUnyqwWU8j2NJca7dR9smuidxpX_YUYAMhPDOrOwpO4ym5GRdFnIKPykpA_Q&usqp=CAU"
           className="h-[3rem] w-30 ml-3"
           alt="Logo"
         />
