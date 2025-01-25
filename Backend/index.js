5a3b3e673c3c2c27c8ebf766b2840d39132e72b8
Raja Dawood <143741655+RajaDawood9@users.noreply.github.com>
2025-01-25 22:19:01 +0500

    Names

--- Backend/index.js
+++ Backend/index.js
@@ -41,7 +41,7 @@ app.options("*", cors(corsOptions)); // Explicitly handle OPTIONS requests
 
 // Routes
 app.get("/", (req, res) => {
-  res.json("Raja Dawood /n Muddassir Khan /n Mahnoor Rasheed");
+  res.json("Raja_Dawood ,Muddassir_Khan ,Mahnoor_Rasheed");
 });
 
 app.use("/api", require("./routes/userRoute.js"));
